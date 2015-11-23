9cb9c8682b062f3a0393b089cee47b856a1c159f
AlexanderDzhoganov <alexander.dzhoganov@gmail.com>
2015-11-23 21:14:44 +0200

    client should reply to heartbeats + socket.io server

--- standalone.js
+++ standalone.js
@@ -1,11 +1,23 @@
 var dgram = require('dgram');
 var net = require('net');
+var io = require('socket.io')();
 
 var discovery = require('./discover');
 var pipdecode = require('./pipdecode');
 var pipdb = require('./pipdb');
 
 var FALLOUT_TCP_PORT = 27000;
+var SOCKETIO_PORT = 3000;
+
+var db = {};
+
+io.listen(SOCKETIO_PORT);
+io.on('connection', function(socket) {
+  console.log('Socket.io client connected!');
+  socket.emit('db_update', db);
+});
+
+console.log("Socket.io server listening on port " + SOCKETIO_PORT);
 
 discovery.discover(function(err, server) {
   if(err) {
@@ -27,24 +39,35 @@ discovery.discover(function(err, server) {
 
   client.on('close', function (hadError) {
     console.log('Connection closed!');
+    process.exit(1);
   });
 
   client.on('end', function () {
-    console.log('Connection closed!');
+    console.log('Connection ended!');
+    process.exit(1);
   });
 
   client.on('error', function (err) {
     console.error('Connection error: ' + err);
+    process.exit(1);
   });
 
   pipdecode.onPacket = function(packet) {
-    if(packet.channel === 1) {
+    if(packet.channel === 0) {
+      var buf = new Buffer(5);
+      for(var i = 0; i < 5; i++) {
+        buf[i] = 0;
+      }
+
+      client.write(buf);
+    } else if(packet.channel === 1) {
       var handshake = JSON.parse(packet.content);
       console.log("Got handshake! Lang: " + handshake.lang + ', Version: ' + handshake.version);
     } else if(packet.channel === 3) {
       pipdb.decodeDBEntries(packet.content);
       db = pipdb.getNormalizedDB();
-      console.log("DB Update!");
+      io.emit('db_update', db);
+      process.stdout.write('.');
     }
   }
 
