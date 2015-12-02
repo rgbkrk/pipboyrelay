536b5065fee12671ae550e0b03518a028cc9fd8a
Kyle Kelley <rgbkrk@gmail.com>
2015-12-02 16:37:51 -0600

    Bring on commander.

--- cli.js
+++ cli.js
@@ -9,11 +9,18 @@ var util = require('util');
 var UDPRelay = relay.UDPRelay;
 var TCPRelay = relay.TCPRelay;
 
+var program = require('commander');
+
+var version = require('./package.json').version;
+
+program
+  .version(version)
+  .option('-t --output-type <type>', 'Output type', /^(hex|decoded)$/i, 'hex')
+  .parse(process.argv)
+
 var maxShowLength = 48;
 
 pipboylib.connection.discover().then(function (server) {
-  console.log('Discovered: ', server);
-
   // Set up a new relay for each running server
 
   var udpRelay = new UDPRelay();
@@ -21,13 +28,19 @@ pipboylib.connection.discover().then(function (server) {
     var t = util.format('%s:%d -> %s:%d',
                         telemetry.src.address, telemetry.src.port,
                         telemetry.dst.address, telemetry.dst.port);
-    var dataChunk = data.slice(0, Math.min(data.length, maxShowLength));
-    var dots = '';
-    if (dataChunk.length < data.length) {
-      dots = '...';
-    }
+
     console.log('[UDP Relay] ', t);
-    console.log(hexy.hexy(dataChunk) + dots);
+    if (program.outputType == 'hex') {
+      var dataChunk = data.slice(0, Math.min(data.length, maxShowLength));
+      var dots = '';
+      if (dataChunk.length < data.length) {
+        dots = '...';
+      }
+      console.log(hexy.hexy(dataChunk) + dots);
+    } else {
+      console.log("TODO: DECODE");
+    }
+
   });
 
   var tcpServerInfo = {};
@@ -41,16 +54,25 @@ pipboylib.connection.discover().then(function (server) {
                         telemetry.src.address, telemetry.src.port,
                         telemetry.dst.address, telemetry.dst.port);
 
-    var dataChunk = data.slice(0, Math.min(data.length, maxShowLength));
-    var dots = '';
-    if (dataChunk.length < data.length) {
-      dots = '...';
-    }
-
     console.log('[TCP Relay] ', t);
-    console.log(hexy.hexy(dataChunk) + dots);
+
+    if (program.outputType == 'hex') {
+      var dataChunk = data.slice(0, Math.min(data.length, maxShowLength));
+      var dots = '';
+      if (dataChunk.length < data.length) {
+        dots = '...';
+      }
+
+      console.log(hexy.hexy(dataChunk) + dots);
+    } else {
+      console.log("TODO: DECODE");
+    }
   });
-  console.log('UDP and TCP Relay created for: ', server.info);
+
+  console.log('UDP and TCP Relay created for:',
+              server.MachineType, 'on', server.info.address)
+  console.log('Connect to: ', tcpRelay.server.address())
+
 }).catch(function(err) {
   throw err;
 });
