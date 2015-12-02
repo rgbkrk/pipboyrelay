5f2f7386cf15d447dbe0063dd1d77c1b1b66371d
Kyle Kelley <rgbkrk@gmail.com>
2015-12-02 16:52:09 -0600

    Log the decoded data.

--- cli.js
+++ cli.js
@@ -16,7 +16,16 @@ var version = require('./package.json').version;
 program
   .version(version)
   .option('-t --output-type <type>', 'Output type', /^(hex|decoded)$/i, 'hex')
-  .parse(process.argv)
+
+program.on('--help', function (){
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ pipboyrelay -t hex');
+  console.log('    $ pipboyrelay -t decoded');
+  console.log('');
+});
+
+program.parse(process.argv)
 
 var maxShowLength = 48;
 
@@ -29,7 +38,7 @@ pipboylib.connection.discover().then(function (server) {
                         telemetry.src.address, telemetry.src.port,
                         telemetry.dst.address, telemetry.dst.port);
 
-    console.log('[UDP Relay] ', t);
+    console.error('[UDP Relay] ', t);
     if (program.outputType == 'hex') {
       var dataChunk = data.slice(0, Math.min(data.length, maxShowLength));
       var dots = '';
@@ -38,7 +47,7 @@ pipboylib.connection.discover().then(function (server) {
       }
       console.log(hexy.hexy(dataChunk) + dots);
     } else {
-      console.log("TODO: DECODE");
+      console.log(JSON.parse(data));
     }
 
   });
@@ -48,13 +57,15 @@ pipboylib.connection.discover().then(function (server) {
   tcpServerInfo.port = pipboylib.constants.FALLOUT_TCP_PORT;
   tcpServerInfo.family = server.info.family;
 
+  var parser = pipboylib.decoding.createDataStream()
+
   var tcpRelay = new TCPRelay();
   tcpRelay.listen(tcpServerInfo, function (data, telemetry) {
     var t = util.format('%s:%d -> %s:%d',
                         telemetry.src.address, telemetry.src.port,
                         telemetry.dst.address, telemetry.dst.port);
 
-    console.log('[TCP Relay] ', t);
+    console.error('[TCP Relay] ', t);
 
     if (program.outputType == 'hex') {
       var dataChunk = data.slice(0, Math.min(data.length, maxShowLength));
@@ -65,13 +76,18 @@ pipboylib.connection.discover().then(function (server) {
 
       console.log(hexy.hexy(dataChunk) + dots);
     } else {
-      console.log("TODO: DECODE");
+      parser.on("readable", function() {
+        var e;
+        while (e = parser.read()) {
+          console.log(e);
+        }
+      });
+      parser.write(data);
     }
   });
 
-  console.log('UDP and TCP Relay created for:',
+  console.error('UDP and TCP Relay created for:',
               server.MachineType, 'on', server.info.address)
-  console.log('Connect to: ', tcpRelay.server.address())
 
 }).catch(function(err) {
   throw err;
